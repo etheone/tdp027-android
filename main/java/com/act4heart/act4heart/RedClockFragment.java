04c7555a5f7300e49a0188308e49ead1bdc4d6e3
Erik Edling <eried975@localhost>
2016-02-04 10:45:11 +0100

    Finished with red clock

--- main/java/com/act4heart/act4heart/RedClockFragment.java
+++ main/java/com/act4heart/act4heart/RedClockFragment.java
@@ -1,6 +1,7 @@
 package com.act4heart.act4heart;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Handler;
@@ -10,14 +11,22 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import java.util.Date;
+
 public class RedClockFragment extends Fragment {
 
-    private OnFragmentInteractionListener mListener;
 
-    private int currentTime = 100000;
+    private int currentTime;
+    private long timeStamp;
+    private boolean continueClock = false;
+
     private  TextView hours;
     private  TextView minutes;
     private  TextView seconds;
+    private  TextView seperatorHour;
+
+    private View view;
+    private SharedPreferences prefs;
 
     public RedClockFragment() {
         // Required empty public constructor
@@ -36,11 +45,32 @@ public class RedClockFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.red_clock_fragment, container, false);
+        view = inflater.inflate(R.layout.red_clock_fragment, container, false);
 
+        //Get saved timerdata
+        prefs = this.getActivity().getSharedPreferences(
+                "timerPrefs", 0);
+
+        //Get current timestamp.
+        timeStamp =  System.currentTimeMillis();
+
+        //If we are continuing the clock in another fragment, we want the same
+        //time to continue. Get the timestamp and give currentTime the correct
+        //passed time.
+        if(continueClock){
+            long time = prefs.getLong("timeStamp", new Date().getTime());
+            currentTime = (int)(System.currentTimeMillis() - time);
+        } else  {
+            prefs.edit().putLong("timeStamp", timeStamp).apply();
+        }
+
+        //Textviews for the clock.
         hours = (TextView) view.findViewById(R.id.hours);
         minutes = (TextView) view.findViewById(R.id.minutes);
         seconds = (TextView) view.findViewById(R.id.seconds);
+
+        //The seperator between hour and minutes
+        seperatorHour = (TextView) view.findViewById(R.id.seperatorHour);
 
         timerHandler.post(timerRunnable);
         return view;
@@ -68,7 +98,6 @@ public class RedClockFragment extends Fragment {
         currentSeconds = currentSeconds - (currentMinutes * 60) - (currentHours * 3600);
 
         //Makes sure that we keep format ex 00:00:00 or 09:20:10
-
         if(currentSeconds > 9){
             seconds.setText(String.valueOf(currentSeconds));
         } else {
@@ -83,33 +112,49 @@ public class RedClockFragment extends Fragment {
 
         if(currentHours > 9){
             hours.setText(String.valueOf(currentHours));
-        } else {
+        } else if( currentHours > 0 ){
             hours.setText("0" + String.valueOf(currentHours));
+
+            //If the timer is below 1 hour, we set the field invisible once
+            if(hours.getVisibility() == View.INVISIBLE) {
+                hours.setVisibility(View.VISIBLE);
+                seperatorHour.setVisibility(View.VISIBLE);
+            }
         }
     }
 
-    public void resetTimer(){
+    //Resets the timer and sets all fields correctly.
+    public void resetTimer() {
+        currentTime = 0;
 
+        //Save the new timestamp
+        timeStamp =  System.currentTimeMillis();
+        prefs.edit().putLong("timeStamp", timeStamp).apply();
+
+
+        hours.setVisibility(View.INVISIBLE);
+        seperatorHour.setVisibility(View.INVISIBLE);
+    }
+
+    //Called from the fragment that implements the clock and want a new timestamp
+    public void saveNewTimeStamp(){
+        continueClock = false;
+    }
+
+    //Called from the fragment that implements the clock and want it to continue
+    public void continueClock(){
+        continueClock = true;
     }
 
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
     }
 
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
     }
 
-    public interface OnFragmentInteractionListener {
-        //void onFragmentInteraction(Uri uri);
-    }
+
 }
