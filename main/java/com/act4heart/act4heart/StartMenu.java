fcfced8510b17b028ab02d238cf646bb05512abd
joagy323@student.liu.se <joagy323@student.liu.se>
2016-02-18 12:42:34 +0100

    Added Toolbar that works on lower APIs to start page

--- main/java/com/act4heart/act4heart/StartMenu.java
+++ main/java/com/act4heart/act4heart/StartMenu.java
@@ -3,6 +3,7 @@ package com.act4heart.act4heart;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,6 +15,10 @@ public class StartMenu extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.start_menu);
+
+        //Add toolbar
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.toolbar2);
+        setSupportActionBar(myToolbar);
 
         Button btnEmergency = (Button) findViewById(R.id.btn_akut);
         btnEmergency.setOnClickListener(new View.OnClickListener() {
