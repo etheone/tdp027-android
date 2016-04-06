5b223c203e71faef89d62191530ed28d47258077
emios530 <emios530@student.liu.se>
2016-04-06 10:58:26 +0200

    added activity about if previous heartproblem without branch

---
+++ main/java/com/act4heart/act4heart/EmergencyQuestions/RelapseQuestionActivity.java
@@ -0,0 +1,53 @@
+package com.act4heart.act4heart.EmergencyQuestions;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+import com.act4heart.act4heart.EmergencySituationActivity;
+import com.act4heart.act4heart.R;
+import com.act4heart.act4heart.Symptoms.SOSCallActivity;
+
+public class RelapseQuestionActivity extends AppCompatActivity {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relapse_question_activity);
+
+        //Add toolbar
+        Toolbar myToolbar = (Toolbar)findViewById(R.id.toolbar2);
+        setSupportActionBar(myToolbar);
+
+        Button buttonRelapse = (Button) findViewById(R.id.btn_relapse);
+        buttonRelapse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startNitroQuestionActivity();
+
+            }
+        });
+        Button buttonNoRelapse = (Button) findViewById(R.id.btn_no_relapse);
+        buttonNoRelapse.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startSOSCallActivity();
+            }
+        });
+    }
+
+    private void startSOSCallActivity(){
+        Intent intent = new Intent(this, SOSCallActivity.class);
+        startActivity(intent);
+    }
+
+    private void startNitroQuestionActivity() {
+        Intent start_relapse_process = new Intent(this, EmergencySituationActivity.class);
+        startActivity(start_relapse_process);
+    }
+}
