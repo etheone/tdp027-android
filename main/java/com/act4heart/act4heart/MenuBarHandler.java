f2a902ce18de78f5278a54e8ca4bb823fe80dca7
Erik Edling <eried975@localhost>
2016-04-21 08:51:00 +0200

    Fixed small crash

--- main/java/com/act4heart/act4heart/MenuBarHandler.java
+++ main/java/com/act4heart/act4heart/MenuBarHandler.java
@@ -23,11 +23,6 @@ public class MenuBarHandler {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
         if (id == R.id.action_sound) {
             if(StartMenu.soundOn) {
                 item.setIcon(R.drawable.ic_volume_off);
